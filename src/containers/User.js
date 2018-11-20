21a7b86870864ef23b6404cc7074d9510ea97bdc
ahammaha <mahalakshmi@outlook.com>
2018-11-20 15:08:07 +0530

    removed node modules and added click event to open conversations of that user

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./User.css";
+import store from "../store";
+import { setActiveUserId } from "../actions";
 
 const User = ({user}) => {
 	const { name, profile_pic, status } = user;
 
 	return(
-		<div className="User">
+		<div className="User" onClick={handleUserClick.bind(null,user)}>
 			<img src={profile_pic} alt={name} className="User_pic" />
 			<div className="User_details">
 				<p className="User_details-name">{name}</p>
@@ -13,6 +15,11 @@ const User = ({user}) => {
 			</div>
 		</div>
 	);
+}
+
+// Use user_id as it is a ES6 shorthand destructuring from user obj passed as parameter 
+function handleUserClick({user_id}){
+	store.dispatch(setActiveUserId(user_id));
 }
 
 export default User;
--- src/reducers/activeUserId.js
+++ src/reducers/activeUserId.js
@@ -1,4 +1,12 @@
+import { SET_ACTIVE_USER_ID } from "../constants/action-types";
+
 
 export default function activeUserId(state=null,action){
-	return state;
+	switch(action.type){
+		case SET_ACTIVE_USER_ID:
+			// new value of activeUserId is set to action.userId
+			return action.userId;
+		default:
+			return state;
+	}
 }
